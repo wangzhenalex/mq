64bd4edf074288fd0bd8bd7d06075c3a195eb157
zhen wang <wangzhen_1990@163.com>
2024-04-08 09:38:49 +0800

    最简单的方式，队列，1v1

--- rabbitmq-hello/src/main/java/org/example/rabbitmqhello/one/Consumer.java
+++ rabbitmq-hello/src/main/java/org/example/rabbitmqhello/one/Consumer.java
@@ -3,20 +3,20 @@ package org.example.rabbitmqhello.one;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 /**
  * Created with IntelliJ IDEA.
  *
  * @author： zhen wang
  * @date： 2024/4/8
- * @description：hello
+ * @description：消费者
  * @version: 1.0
  */
-public class Producer {
-    //    队列名称
+public class Consumer {
     private final static String QUEUE_NAME = "hello";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // 创建连接工厂
         ConnectionFactory factory = new ConnectionFactory();
         // 设置RabbitMQ地址
@@ -29,12 +29,19 @@ public class Producer {
         try (Connection connection = factory.newConnection();
              // 创建一个通道
              Channel channel = connection.createChannel()) {
-            // 声明一个队列
+            // 声明要消费的队列
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            // 发送消息到队列中
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            System.out.println("消息发送完毕 [x] Sent '" + message + "'");
+            System.out.println(" [*] Waiting for messages. To exit press Ctrl+C");
+            // 创建队列消费者
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println("消息消费完毕 [x] Received '" + message + "'");
+            };
+            // 消费消息
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
